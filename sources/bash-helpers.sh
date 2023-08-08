7d9f98de6bb02d75c7397a26d80c55ce9da713a0
Glen Goulter <glenrg@uw.edu>
2023-08-08 13:21:38 -0700

    Align locally run deploy timestamps with UTC time.

--- sources/bash-helpers.sh
+++ sources/bash-helpers.sh
@@ -3,5 +3,7 @@
 function tag_timestamp {
   # This timestamp format can be lexicographically sorted
   # which is important for flux automation.
-  echo $(date +%Y.%m.%d.%H.%M.%S)
+  # This is in UTC so that timestamps will be the same,
+  # whether this script is run directly from a developer's machine or from a github action
+  echo $(date -u +%Y.%m.%d.%H.%M.%S)
 }
